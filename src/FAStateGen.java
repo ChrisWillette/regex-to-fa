e1cd449fcb3e3c1ec5d7de795f87e0a49cd0d8cf
Christopher Willette <christopher.willette@gmail.com>
2015-10-04 22:11:39 -0700

    init

---
+++ src/FAStateGen.java
@@ -0,0 +1,15 @@
+/**
+ * @author Chris Willette
+ * SID 11444663
+ */
+public class FAStateGen {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
