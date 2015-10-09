4fc5fa6f3c57f93a44e27deeb1eb769d8fe2fd83
Christopher Willette <christopher.willette@gmail.com>
2015-10-08 18:06:47 -0700

    working-ish. state and fa decs suck and are ugly. i blame the cat.

--- src/FAStateGen.java
+++ src/FAStateGen.java
@@ -8,26 +8,42 @@ import java.lang.*;
 import java.io.*;
 
 
-@SuppressWarnings("unused")
+@SuppressWarnings("unused")//looking at you, state.
 public class FAStateGen {
 
 	public int stateCount = 0;
 	private final FA result;
 	
-	private class FA{
+	private class FA {
 		public ArrayList<State> stateList = new ArrayList<State>();
-		public State start;
-		public State end;		
+		int start; //used when chaining fa state lists together
+		int end;  //same
+		Boolean isEmpty; //deals with empty set
 	}
 	
-	private class State{
-		int number = stateCount;
+//later code would have been much less painful had i created proper
+	//constructors for state and fa.
+
+	
+	
+	private class State implements Comparable<State> {
+		int number;
 		char input;
-		State next;
-		Boolean empty = false;
-		Boolean isStart = false;
-		Boolean isEnd = false;
-	}
+		int next;
+		Boolean isStart; //flag set for printing purposes
+		Boolean isEnd;  //same		
+	
+	@Override
+	public int compareTo(State other){
+		if(this.number == other.number)		
+			return 0;
+		else if (this.number < other.number)
+			return -1;
+		else
+			return 1;
+	}	
+}	
+	
 	
 	
 	public FAStateGen(String line){
@@ -45,60 +61,212 @@ public class FAStateGen {
 		FA machine = new FA();
 		for(int i = 0; i < line.length(); i++) {
 			char input = line.charAt(i);
+			
+			//test input
 			if(st.size() <2 && ((input == '+')||(input == '&')) ){
 				System.err.println("invalid input: " + line);
 				stateCount = 0; //notifies caller of bad input
 				break;
 			}
+			
 			if(input == '&'){//fa1&fa2
 				FA fa2 = st.pop();
 				FA fa1 = st.pop();
+				if (fa1.isEmpty == true || fa2.isEmpty == true){
+					continue;
+				}
 				FA faNew = new FA();
 				faNew.stateList.addAll(fa1.stateList);
 				State newState = new State();
-				newState.number = fa1.end.number;
+				newState.number = fa1.end;
 				newState.input = 'E';
-				newState.next.number = fa2.start.number;
+				newState.next = fa2.start;
 				faNew.stateList.add(newState);
 				faNew.stateList.addAll(fa2.stateList);
-				faNew.start.number = fa1.start.number;
+				faNew.start = fa1.start;
 				faNew.end = fa2.end;
+				//now cleaning up start/end flags
+				for(int j = 0; j < faNew.stateList.size(); j++){
+					if(faNew.stateList.get(j).number !=  faNew.start)
+						faNew.stateList.get(j).isStart = false;
+					else faNew.stateList.get(j).isStart = true;
+					if(faNew.stateList.get(j).number !=  faNew.end)
+						faNew.stateList.get(j).isEnd = false;
+					else faNew.stateList.get(j).isEnd = true;
+				}
+				faNew.isEmpty = false;
 				st.push(faNew);
+				
+				
 			}else if(input == '+'){//fa1+fa2
 				FA fa2 = st.pop();
 				FA fa1 = st.pop();
+				if(fa1.isEmpty == true && fa2.isEmpty == true) {
+					//we're just gonna act like it's an input of empty set
+					stateCount++;
+					State ns1 = new State();
+					ns1.number = stateCount;
+					ns1.next = stateCount;
+					ns1.input = 'E';
+					ns1.isEnd = true;
+					ns1.isStart = true;				
+					FA fa = new FA();
+					fa.stateList.add(ns1);				
+					fa.start = ns1.number;
+					fa.end = ns1.number;
+					fa.isEmpty = true;
+					st.push(fa);					
+					continue;
+				}else if(fa1.isEmpty == true){
+					st.push(fa2);//0+fa2 = fa2					
+					continue;
+				}
+				else if(fa2.isEmpty == true){
+					st.push(fa1);//fa1+0 = fa1					
+					continue;
+				} 				
 				FA faNew = new FA();
 				stateCount++;
 				State pre = new State();
-				faNew.start = pre;				
-				pre.next.number = fa1.start.number;
+				pre.number = stateCount; //test if needed
+				faNew.start = pre.number;				
+				pre.next = fa1.start;
 				pre.input = 'E';
-				faNew.stateList.add(pre);
-				
-				State pre2 = new State();
-				pre2.next.number = fa2.start.number;
+				faNew.stateList.add(pre);				
+				State pre2 = new State(); 
+				pre2.number = stateCount; //test if needed
+				pre2.next = fa2.start;
 				pre2.input = 'E';
 				faNew.stateList.add(pre2);
+				faNew.stateList.addAll(fa1.stateList);
+				faNew.stateList.addAll(fa2.stateList);
+				stateCount++;
+				State post1 = new State();
+				State post2 = new State();
+				post1.number = fa1.end;
+				post2.number = fa2.end;
+				post1.input = 'E';
+				post2.input = 'E';
+				post1.next = stateCount;
+				post2.next = stateCount;
+				faNew.stateList.add(post1);
+				faNew.stateList.add(post2);
+				faNew.end = stateCount;
+				//now cleaning up start/end flags
+				faNew.isEmpty = false;
+				for(int j = 0; j < faNew.stateList.size(); j++){
+					if(faNew.stateList.get(j).number !=  faNew.start)
+						faNew.stateList.get(j).isStart = false;
+					else faNew.stateList.get(j).isStart = true;
+					if(faNew.stateList.get(j).number !=  faNew.end)
+						faNew.stateList.get(j).isEnd = false;
+					else faNew.stateList.get(j).isEnd = true;
+				}
+				st.push(faNew);
 				
-		
+			}else if(input == '*'){//fa*				
+				FA fa = st.pop();
+				stateCount++;
+				State pre = new State();
+				State post = new State();
+				FA faNew = new FA();
+				pre.input = 'E';
+				post.input = 'E';
+				pre.number = stateCount;
+				post.number = fa.end;
+				pre.next = fa.start;
+				post.next = pre.number;
+				pre.isEnd = true;
+				pre.isStart = true;
+				faNew.stateList.add(pre);
+				faNew.stateList.addAll(fa.stateList);
+				faNew.stateList.add(post);
+				//kleene house (lololol)
+				for(int j = 0; j < faNew.stateList.size(); j++){
+					if(faNew.stateList.get(j).number !=  faNew.start)
+						faNew.stateList.get(j).isStart = false;
+					else faNew.stateList.get(j).isStart = true;
+					if(faNew.stateList.get(j).number !=  faNew.end)
+						faNew.stateList.get(j).isEnd = false;
+					else faNew.stateList.get(j).isEnd = true;
+				}
+				faNew.isEmpty = false;
+				st.push(faNew);
 				
-			}else if(input == '*'){
-				
-				
-				
-				
+			}else if(input == '0'){//fa for empty set
+				stateCount++;
+				State ns1 = new State();
+				ns1.number = stateCount;
+				ns1.next = stateCount;
+				ns1.input = 'E';
+				ns1.isEnd = true;
+				ns1.isStart = true;				
+				FA fa = new FA();
+				fa.stateList.add(ns1);				
+				fa.start = ns1.number;
+				fa.end = ns1.number;
+				fa.isEmpty = true;
+				st.push(fa);
 			}else{
-				
-				
-				
-				
+				stateCount++;
+				State ns1 = new State();
+				State ns2 = new State();
+				ns1.number = stateCount;
+				stateCount++;
+				ns1.next = stateCount;
+				ns1.input = 'E';
+				ns1.isEnd = false;
+				ns1.isStart = true;
+				ns2.number = ns1.next;
+				ns2.input = input;
+				ns2.isEnd = true;
+				ns2.isStart = false;
+				FA fa = new FA();
+				fa.stateList.add(ns1);
+				fa.stateList.add(ns2);
+				fa.start = ns1.number;
+				fa.end = ns2.number;
+				fa.isEmpty = false;
+				st.push(fa);
 			}			
 		}
 		if(st.size() > 1){		
 			System.err.println("invalid input: " + line);
 			stateCount = 0;
-		}return machine;
-		
+		}
+		machine = st.pop();
+		return machine;		
+	}
+	
+	
+	
+	
+
+	private void printTable(String name) {
+		try{
+			String home = System.getProperty("user.home");
+			name = home + "\\Desktop\\" + name;
+			File out = new File(name);
+			if(!out.exists()) out.createNewFile();
+			Collections.sort(result.stateList);
+			FileWriter fw = new FileWriter(out.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			for(int i = 0; i < result.stateList.size(); i++){
+				String entry = "\r\n" + "(q" + result.stateList.get(i).number +  " , " +
+						result.stateList.get(i).input + ") --> " +
+						result.stateList.get(i).next;				
+				if(result.stateList.get(i).isStart == true)
+					entry = " S";
+				if(result.stateList.get(i).isEnd == true)
+					entry = entry + " F";		
+				//entry = entry + "\r\n";
+				bw.write(entry);
+			}
+			bw.close();
+			
+		}catch (IOException e){
+			System.err.println(e.getMessage());
+		}
 	}
 	
 	
@@ -110,31 +278,28 @@ public class FAStateGen {
 		System.out.print("enter file name: ");
 		System.out.flush();
 		String filename = scanner.nextLine();
-
-
 		try{
 			File file = new File(filename);
 			FileReader fileReader = new FileReader(file);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			StringBuffer stringBuffer = new StringBuffer();
 			String line;
 			int lineNumber = 0;
 			while ((line = bufferedReader.readLine()) != null) {
 				if(line.isEmpty())continue;
 				lineNumber++;
-				FAStateGen automata = new FAStateGen(line);
-				if(automata.stateCount == 0) continue; //skip past bad input
-				String name = "\"FA" + lineNumber + ".txt\"";
-				FileOutputStream out = new FileOutputStream(name);
-				FAPrinter(automata.getResult());
-				out.close();
+				FAStateGen automata = new FAStateGen(line);				
+				String name = "willette-FA" + lineNumber + ".txt";
+				if(automata.stateCount == 0) continue; //skip past bad input				
+				automata.printTable(name);
+				
 			}
-			fileReader.close();
-			
+			fileReader.close();			
 		}catch (IOException e) {
 			System.err.println(e.getMessage());
+		}finally{
+			scanner.close();
 		}
-	}
+	}//end main
 }
 
 
